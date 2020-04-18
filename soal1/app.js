68c1a54469218d36f2f2f3afc25a2e0573d5e899
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-04-18 16:06:59 +0700

    Update app.js

---
+++ soal1/app.js
@@ -0,0 +1,57 @@
+const express    = require('expres');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/v1/biodata/:name/:age", (req, res) => {
+    let name = req.params.name;
+    let age  = req.params.age;
+    
+    if (name != 'luthfi' || age != 24) {
+        return res.status(404).send({error: true, message: 'Bukan biodata saya'});
+    }
+
+    res.json({
+        name: 'Luthfi Widya Ekakusuma',
+        age: 24,
+        address: 'GKP Blok G4 No 19, Argorejo, Sedayu, Bantul, Yogyakarta',
+        hobbies:['Seni bela diri', 'Berenang'],
+        is_married: false,
+        list_school: [
+            {
+                name: 'SDN Suryodiningratan III Yogyakarta',
+                year_in: '2002',
+                year_out: '2008'
+            },
+            {
+                name: 'SMP N 4 Gamping Sleman',
+                year_in: '2008',
+                year_out: '2011'
+            },
+            {
+                name: 'SMK Muhammadiyah 2 Yogyakarta',
+                year_in: '2011',
+                year_out: '2014'
+            },
+            {
+                name: 'AMIK BSI YOGYAKARTA',
+                year_in: '2014',
+                year_out: '2017'
+            
+            }],
+        skills:[
+            {
+                skill_name: 'Web Programing',
+                level: 'advanced'
+            },
+            {
+                skill_name: 'Server Administration',
+                level: 'advanced'
+            }
+        ],
+        interest_in_coding: true
+            
+    });
+
+});
